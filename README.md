b4c9a67fd17834541a137eccfde209f962cf8247
Marcos <mbinoti@gmail.com>
2024-05-02 11:18:15 -0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# goal_list
+
+A new Flutter project.
+# goal_list
---
+++ android/app/src/main/kotlin/br/com/mbi/goal_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.mbi.goal_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
